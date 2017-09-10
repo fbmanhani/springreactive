40e733e3b25b3db845bf8177b1664769025cd2c9
fbmanhani <fbmanhani@gmail.com>
2017-09-10 15:33:51 -0300

    Initial commit

---
+++ src/main/java/br/com/manhani/springreactive/SpringreactiveApplication.java
@@ -0,0 +1,12 @@
+package br.com.manhani.springreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactiveApplication.class, args);
+	}
+}
